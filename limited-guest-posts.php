a54568d3017dafc58a20e2484efbf6bce53f15a3
Mehul <mehul.kaklotar@gmail.com>
2016-12-01 21:23:01 +0530

    plugin main file add

---
+++ limited-guest-posts.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Limited Guest Posts
+ * Plugin URI: https://github.com/mehulkaklotar/limited-guest-posts
+ * Description: Limit the posts for guests to view
+ * Version: 1.0
+ * Author: mehulkaklotar
+ * Author URI: http://kaklo.me
+ * Requires at least: 4.1
+ * Tested up to: 4.5
+ *
+ * Text Domain: limited-guest-posts
+ *
+ * @package Limited_Guest_Posts
+ * @category Core
+ * @author mehulkaklotar
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
